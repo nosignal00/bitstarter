619013b607826d1df38a365fa90184d7314f5372
Bang <nosignal00@gmail.com>
2013-07-10 16:21:56 +0000

    web.js modified for applying index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
+
+var content = fs.readFileSync("./index.html");
+
+var string_content = content.toString();
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(string_content);
 });
 
 var port = process.env.PORT || 5000;
